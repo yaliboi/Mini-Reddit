09e0add0f776bf9ea6ff9facb1e5f96ec5f4a4d7
boazikum <43822948+boazikum@users.noreply.github.com>
2024-08-15 22:53:35 +0300

    uploded all back-end files

---
+++ back-end/PgConnector.js
@@ -0,0 +1,33 @@
+const { Client } = require('pg');
+
+async function useDb(query, queryParameters=[]) {
+  const client = new Client({
+    connectionString: "postgresql://postgres:postgres@localhost:5432/minireddit?schema=public"
+  });
+
+  let results = [];
+
+  try {
+    console.log("before connect");
+    await client.connect();
+    console.log("after connect");
+    const res = await client.query(query, queryParameters);
+    results = res.rows;
+    console.log("connected, results:", results);
+  } catch (err) {
+    console.error(err);
+  }
+  await client.end();
+  return results;
+}
+
+const queryDb = (query, queryParameters = []) => {
+    return useDb(query, queryParameters).then(results => {
+        return results
+    }).catch(err => {
+        console.log(err)
+        return []
+    })
+}
+
+module.exports = queryDb;
---
+++ back-end/app.js
@@ -0,0 +1,69 @@
+const queryDb = require('./PgConnector');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json(), cors())
+
+/*app.get(), app.post(), app.put(), app.delete() */
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.get('/api/posts', async (req, res) => {
+    let posts = await queryDb("SELECT * FROM full_post_info;");
+    console.log(posts)
+    res.send(posts)
+})
+
+app.get('/api/posts/:id', async (req, res) => {
+    let post = await queryDb("SELECT * FROM full_post_info where id=$1;", [req.params.id]);
+    if (post == undefined || post.length == 0){
+        res.status(404).send('Post not found');
+    }else{    
+        res.send(post);
+    }
+})
+
+app.post('/api/posts', async (req, res) => {
+    const query = `INSERT INTO public.posts(authorid, title, body)VALUES ($1, $2, $3) returning id, authorid, title, body;`
+    let newPost = await queryDb(query, [parseInt(req.body.authorid, 10), req.body.title, req.body.body]);
+    res.send(newPost);
+})
+
+app.delete('/api/posts/:id', async (req, res) => {
+    let post = await queryDb("DELETE FROM posts where id=$1 returning id, authorid, title, body;", [req.params.id])
+    if (post == undefined || post.length == 0){
+        res.status(404).send('Post not found');
+    }else{    
+        res.send(post);
+    }
+})
+
+app.get('/api/users', async (req, res) => {
+    let posts = await queryDb("SELECT * FROM public.users;");
+    console.log(posts)
+    res.send(posts)
+})
+
+app.get('/api/users/:id', async (req, res) => {
+    let post = await queryDb("SELECT * FROM public.users where id=$1;", [req.params.id]);
+    if (post == undefined || post.length == 0){
+        res.status(404).send('Post not found');
+    }else{    
+        res.send(post);
+    }
+})
+app.get('/api/comments', (req, res) => {
+    res.send(req.query); // displays the url query aka ?id=4&sorby=id:desc
+})
+
+app.post('/api/comments', async (req, res) => {
+    const query = `INSERT INTO public.comments(postid, authorid, body)VALUES ($1, $2, $3) returning id, authorid, title, body;`;
+    let newComment = await queryDb(query, [parseInt(req.body.postid), parseInt(req.body.authorid).title, req.body.body]);
+    res.send(newComment);
+})
+
+   
+app.listen(3000, () => console.log('listening on port 3000'));
